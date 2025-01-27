d8a9bc366d55f632840b6e049575217a1a1e821c
Pandaluf <juliobarrios0296@gmail.com>
2025-01-27 18:53:31 -0500

    fix: base url edited in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/JesusReparador_LandingPage",
+  base: "https://pandaluf.github.io/JesusReparador_LandingPage/",
 })
