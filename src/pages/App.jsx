530aa6f319386f5c1882ba22549fbe542147c240
Pandaluf <juliobarrios0296@gmail.com>
2025-01-27 19:21:37 -0500

    fix: Testing responsive

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -13,7 +13,6 @@ function App() {
       <h1>¡Bienvenido al colegio "Jesús Reparador"!</h1>
       <h1>Donde los sueños de tus hijos se convierten en nuestra misión</h1>
     </div>
-    <Footer />
     </>
   )
 }
