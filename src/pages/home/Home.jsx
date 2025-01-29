0ad94a943ce1ef0aa0bd9e03e609c03597472540
Pandaluf <juliobarrios0296@gmail.com>
2025-01-29 11:22:42 -0500

    feat: Images section added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,7 +6,7 @@ import "./Home.css";
 function Home() {
     return (
         <>
-            <CarouselFade />
+            <CarouselFade/>
             <section className="welcome text-center">
                 <h1>¡Bienvenido al colegio Jesús Reparador!</h1>
                 <h2>Donde los sueños de tus hijos se convierten en nuestra misión</h2>
@@ -19,31 +19,83 @@ function Home() {
                 <div className="d-flex justify-content-around flex-wrap gap-2 mb-4 w-100">
                     <CardBasic title="Formación de Valores"
                                text="Fomentamos la responsabilidad, el respeto y la solidaridad, formando no solo estudiantes brillantes, sino también ciudadanos íntegros."
-                               img={imgHome.imgIntegrity} />
+                               img={imgHome.imgIntegrity}/>
                     <CardBasic title="Infraestructura Moderna"
                                text="Pizarra interactiva, laboratorio, sala de computo y espacios recreativos diseñados para el desarrollo integral de nuestros alumnos."
-                               img={imgHome.imgPlayground} />
+                               img={imgHome.imgPlayground}/>
                     <CardBasic title="Participaciones en Concursos"
                                text="Nuestros estudiantes participan en competencias científicas, olimpiadas de matemáticas y concursos de arte locales y nacionales."
-                               img={imgHome.imgTrophy} />
+                               img={imgHome.imgTrophy}/>
                     <CardBasic title="Actividades Extracurriculares"
                                text="Ofrecemos una variedad de actividades como deportes, música, arte, danza y más!, promoviendo el talento y trabajo en equipo."
-                               img={imgHome.imgDance} />
+                               img={imgHome.imgDance}/>
                 </div>
 
                 {/* Segunda fila: 3 tarjetas centradas */}
-                <div className="d-flex justify-content-around flex-wrap gap-2 w-75">
+                <div className="d-flex justify-content-evenly flex-wrap gap-2 w-100">
                     <CardBasic title="Excelencia Académica"
-                               text="Brindamos educación de calidad con un currículo actualizado, enfocado en el desarrollo intelectual y rendimiento académico de cada alumno."
-                               img={imgHome.imgDiploma} />
+                               text="brindamos educación de calidad con un currículo actualizado, enfocado en el desarrollo intelectual y rendimiento académico de cada alumno."
+                               img={imgHome.imgDiploma}/>
                     <CardBasic title="Material Educativo"
                                text="Ofrecemos una amplia variedad de materiales educativos físicos que fomentan un aprendizaje efectivo y motivador."
-                               img={imgHome.imgDiploma} />
+                               img={imgHome.imgDiploma}/>
                     <CardBasic title="Seguridad y Bienestar"
                                text="Priorizamos la seguridad con personal capacitado, cámaras de seguridad y protocolos de emergencia para proteger a nuestros estudiantes."
-                               img={imgHome.imgDiploma} />
+                               img={imgHome.imgDiploma}/>
                 </div>
             </section>
+
+
+            <section className="mt-4 mb-1">
+                <h1 className="text-center">Celebraciones y Eventos Especiales</h1>
+                <div className="event__mosaic text">
+                    <div
+                        className="event__mosaic__card card-tall card-wide"
+                    ><img className="card-custom-image" src={imgHome.imgAnniversary1}/></div>
+                    <div
+                        className="event__mosaic__card"
+                    ><div
+                        className="event__mosaic__card"
+                    ><img className="card-custom-image" src={imgHome.imgAchievementDay5}/></div></div>
+                    <div
+                        className="event__mosaic__card"
+                    ><img className="card-custom-image" src={imgHome.imgAnniversary2}/></div>
+                    <div
+                        className="event__mosaic__card card-wide"
+                    ><img className="card-custom-image" src={imgHome.imgAchievementDay3}/></div>
+                    <div
+                        className="event__mosaic__card"
+                    ><img className="card-custom-image" src={imgHome.imgAchievementDay2}/></div>
+                    <div
+                        className="event__mosaic__card"
+                    ><img className="card-custom-image" src={imgHome.imgOlympics1}/></div>
+                    <div
+                        className="event__mosaic__card card-wide"
+                    ><img className="card-custom-image" src={imgHome.imgAchievementDay4}/></div>
+                    <div
+                        className="event__mosaic__card"
+                    ><img className="card-custom-image" src={imgHome.imgAchievementDay6}/></div>
+                    <div
+                        className="event__mosaic__card"
+                    ><img className="card-custom-image" src={imgHome.imgPeasantsDay1}/></div>
+                    <div
+                        className="event__mosaic__card"
+                    ><img className="card-custom-image" src={imgHome.imgPeasantsDay2}/></div>
+                    <div
+                        className="event__mosaic__card"
+                    ><img className="card-custom-image" src={imgHome.imgSchoolDrill1}/></div>
+                    <div
+                        className="event__mosaic__card card-wide"
+                    ><img className="card-custom-image" src={imgHome.imgSchoolDrill2}/></div>
+                    <div
+                        className="event__mosaic__card card-wide"
+                    ><img className="card-custom-image" src={imgHome.imgParaliturgy1}/></div>
+                    <div
+                        className="event__mosaic__card"
+                    ><img className="card-custom-image" src={imgHome.imgSwearingIn1}/></div>
+                </div>
+            </section>
+
         </>
     );
 }
--- src/pages/home/HomeImages.js
+++ src/pages/home/HomeImages.js
@@ -3,6 +3,30 @@ import Playground from "../../assets/images/Playground.png"
 import Trophy from "../../assets/images/Trophy.png"
 import Dance from "../../assets/images/Dance.png"
 import Diploma from "../../assets/images/Diploma.png"
+import AchievementDay1 from "../../assets/images/news/Achievement_Day_1.jpg"
+import AchievementDay2 from "../../assets/images/news/Achievement_Day_2.jpg"
+import AchievementDay3 from "../../assets/images/news/Achievement_Day_3.jpg"
+import AchievementDay4 from "../../assets/images/news/Achievement_Day_4.jpg"
+import AchievementDay5 from "../../assets/images/news/Achievement_Day_5.jpg"
+import AchievementDay6 from "../../assets/images/news/Achievement_Day_6.jpg"
+import Anniversary1 from "../../assets/images/news/Anniversary_1.jpg"
+import Anniversary2 from "../../assets/images/news/Anniversary_2.jpg"
+import ChildrenDay1 from "../../assets/images/news/Children's_Day_1.jpg"
+import ChildrenDay2 from "../../assets/images/news/Children's_Day_2.jpg"
+import ChildrenDay3 from "../../assets/images/news/Children's_Day_3.jpg"
+import Minichef1 from "../../assets/images/news/Minichef_1.jpg"
+import Olympics1 from "../../assets/images/news/Olympics_1.jpg"
+import Olympics2 from "../../assets/images/news/Olympics_2.jpg"
+import Paraliturgy1 from "../../assets/images/news/Paraliturgy_1.jpg"
+import PeasantsDay1 from "../../assets/images/news/Peasant's_Day_1.jpg"
+import PeasantsDay2 from "../../assets/images/news/Peasant's_Day_2.jpg"
+import SchoolDrill1 from "../../assets/images/news/School_Drill_1.jpg"
+import SchoolDrill2 from "../../assets/images/news/School_Drill_2.jpg"
+import Welcome1 from "../../assets/images/news/Welcome_1.jpg"
+import Welcome2 from "../../assets/images/news/Welcome_2.jpg"
+import WorldAutismDay1 from "../../assets/images/news/World_Autism_Day_1.jpg"
+import SwearingIn1 from "../../assets/images/news/SwearingIn_1.jpg"
+
 
 export default {
     "imgIntegrity":Integrity,
@@ -10,4 +34,27 @@ export default {
     "imgTrophy": Trophy,
     "imgDance": Dance,
     "imgDiploma": Diploma,
+    "imgAchievementDay1": AchievementDay1,
+    "imgAchievementDay2": AchievementDay2,
+    "imgAchievementDay3": AchievementDay3,
+    "imgAchievementDay4": AchievementDay4,
+    "imgAchievementDay5": AchievementDay5,
+    "imgAchievementDay6": AchievementDay6,
+    "imgAnniversary1": Anniversary1,
+    "imgAnniversary2": Anniversary2,
+    "imgChildrenDay1": ChildrenDay1,
+    "imgChildrenDay2": ChildrenDay2,
+    "imgChildrenDay3": ChildrenDay3,
+    "imgMinichef1": Minichef1,
+    "imgOlympics1": Olympics1,
+    "imgOlympics2": Olympics2,
+    "imgParaliturgy1": Paraliturgy1,
+    "imgPeasantsDay1": PeasantsDay1,
+    "imgPeasantsDay2": PeasantsDay2,
+    "imgSchoolDrill1": SchoolDrill1,
+    "imgSchoolDrill2": SchoolDrill2,
+    "imgWelcome1": Welcome1,
+    "imgWelcome2": Welcome2,
+    "imgWorldAutismDay1": WorldAutismDay1,
+    "imgSwearingIn1": SwearingIn1,
 }
