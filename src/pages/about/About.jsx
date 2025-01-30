ed5bb102687120eac4133b37a0ec85c5fe54cb44
Pandaluf <juliobarrios0296@gmail.com>
2025-01-30 17:25:01 -0500

    feat: Mission and Vision added in About page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,6 +1,8 @@
 import "./About.css"
 import Section from "../../components/section/Section.jsx";
-import BadjeJR from "../../assets/images/Badge_JR.jpg"
+import BadjeJR from "../../assets/images/Badge_JR.jpg";
+import MissionJR from "../../assets/images/Mission_JR.png";
+import VissionJR from "../../assets/images/Vision_JR.jpg";
 
 function About() {
     return (
@@ -16,16 +18,12 @@ function About() {
                             está dedicado a acompañar a los alumnos en cada etapa de su aprendizaje, asegurando que adquieran las
                             herramientas necesarias para enfrentar con éxito los retos del futuro.
                         </p>
-                        <br />
-                        <p>
-                            Contamos con instalaciones adaptadas a las necesidades de cada nivel educativo: inicial, primaria
-                            y secundaria. Nuestros laboratorios, talleres y espacios recreativos están diseñados para promover un
-                            aprendizaje dinámico y participativo. En el Colegio Jesús Reparador, cultivamos valores de respeto,
-                            colaboración y trabajo en equipo, creando una comunidad educativa que apoya a cada estudiante en su
-                            crecimiento personal y académico.
-                        </p>
                     </div>
                     <img src={BadjeJR} alt="BadjeJR" className="who__content__img"/>
+                </div>
+                <div className="d-flex justify-content-evenly mt-5 mb-2">
+                    <img src={MissionJR} alt="MissionJR" />
+                    <img src={VissionJR} alt="MissionJR"/>
                 </div>
             </section>
         </>
