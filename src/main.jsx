b03481f8be2efb85dc851353678abcf14f5356ef
Pandaluf <juliobarrios0296@gmail.com>
2025-01-30 09:34:27 -0500

    fix: Testing HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './pages/App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter} from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // 👈 Usa HashRouter en lugar de BrowserRouter
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <BrowserRouter basename="/JesusReparador_LandingPage">
-          <App />
-      </BrowserRouter>
-  </StrictMode>,
-)
+    <StrictMode>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </StrictMode>
+);
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import NavBar from "../components/navbar/NavBar.jsx";
 import Footer from "../components/footer/Footer.jsx";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 import Home from "./home/Home.jsx";
 import About from "./about/About.jsx";
 import Contact from "./contact/Contact.jsx";
